97dfdc9cc2594f78c6655309c138fadd3882dc09
evgenijbelov <beles123@bk.ru>
2022-11-25 12:23:11 +0300

    z13

--- z13/Program.cs
+++ z13/Program.cs
@@ -4,13 +4,11 @@
 Console.Clear();
 Console.WriteLine("Напишите число");
 int a = int.Parse(Console.ReadLine());
-
     if (a <100)
-Console.WriteLine("третьей цифры нет");
-
+Console.Write("третьей цифры нет");
     while(a >=1000)
 {
  a =a/10;
 }
 int a1 = a%10;
-Console.WriteLine(a1);
+Console.Write(a1);
